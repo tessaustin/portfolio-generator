42a0c5cde7eb1d4b2489550f04d7a8ed3aeee93c
Tess Austin <tessaustin92@gmail.com>
2021-03-12 12:38:14 -0700

    updated code

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
-    }
-    
-    console.log('================');
+  }
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+  console.log('================');
 
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
-  
-  printProfileData(profileDataArgs);
+
+printProfileData(profileDataArgs);
+
 
 
